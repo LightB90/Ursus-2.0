2dfedc217f4f98e88dd7c8c8089a08335d43f7e6
silviu <visan.silviu@fortunagroup.eu>
2021-03-29 02:41:28 +0300

    Update 4.3

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,6 +4,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="Description" content="Standarde Ursus Breweries">
+    <meta name="theme-color" content="#317EFB"/>
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <title>{{ config('app.name', 'Laravel') }}</title>
