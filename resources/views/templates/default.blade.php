9252ff98c8c7880e680c4090b8074ad5dec805d7
silviu <visan.silviu@fortunagroup.eu>
2021-03-29 03:16:41 +0300

    Update 4.31

--- resources/views/templates/default.blade.php
+++ resources/views/templates/default.blade.php
@@ -5,10 +5,10 @@
                 <nav class="menu">
                     <input type="checkbox" class="menu-open" name="menu-open" id="menu-open" value="true">
                     <label class="menu-open-button" @if($page->id != '1') data-page=" {{$page->parents->id}}" @endif for="menu-open">
-                        <img alt="MT" width="60" height="60" class="back_img" src="@if($page->id == '1') {{asset('images/home_asahi.svg')}} @else {{asset('images/back.svg')}}@endif">
+                        <img alt="MT" class="back_img" src="@if($page->id == '1') {{asset('images/home_asahi.svg')}} @else {{asset('images/back.svg')}}@endif">
                     </label>
                     @foreach($page->childrens as $child)
-                        <a class="menu-item" data-page="{{$child->id}}"><img alt="" src="{{asset('images/icons/'.$child->picture)}}"></a>
+                        <a class="menu-item" data-page="{{$child->id}}"><img alt="button" width="60px" height="60px" src="{{asset('images/icons/'.$child->picture)}}"></a>
                     @endforeach
                 </nav>
             </div>
