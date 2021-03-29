13aab0dddb4020c5c1e7b369885d5296873086a2
silviu <visan.silviu@fortunagroup.eu>
2021-03-29 03:35:32 +0300

    Update assets -> inline add

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -11,10 +11,7 @@
     <title>{{ config('app.name', 'Laravel') }}</title>
 
     <!-- Scripts -->
-    <script src="{{asset('js/jquery-3.3.1.js')}}"></script>
-    <script src="{{asset('js/popper.min.js')}}"></script>
-    <script src="{{asset('js/bootstrap.min.js')}}"></script>
-    <script src="{{asset('js/toastr.min.js')}}"></script>
+    @include('assets.scripts')
 
     <!-- Styles -->
     <link rel="stylesheet" type="text/css" href="{{asset('css/bootstrap.min.css')}}">
