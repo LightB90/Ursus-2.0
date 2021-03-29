7e64d58718b5bab70176dc240f57e695b9860fef
silviu <visan.silviu@fortunagroup.eu>
2021-03-29 15:45:33 +0300

    update scripts and home.blade

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,6 +2,8 @@
 @push('js-css')
     <link rel="stylesheet" href="{{asset('css/home.css')}}">
     <link rel="stylesheet" href="{{asset('css/5buttons.css')}}">
+    <link href="{{asset('css/summernote-bs4.min.css')}}" rel="stylesheet">
+    <script src="{{asset('js/summernote-bs4.min.js')}}"></script>
 @endpush
 @section('content')
     <div class="main-content">
